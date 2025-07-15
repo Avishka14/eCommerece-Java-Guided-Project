9aba01ccd7d507f1c4c1c67820042aca71cc8527
Avishka Chamod <avishkachamod14@gmail.com>
2025-07-15 12:02:22 +0530

    commit

---
+++ src/java/controller/SignUp.java
@@ -0,0 +1,35 @@
+
+package controller;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "SignUp", urlPatterns = {"/SignUp"})
+public class SignUp extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+         Gson gson = new Gson();
+         JsonObject user = gson.fromJson(request.getReader(), JsonObject.class);
+         
+         String firstName = user.get("firstName").getAsString();
+         String lastName = user.get("lastName").getAsString();
+         String email = user.get("email").getAsString();
+         String password = user.get("password").getAsString();
+        
+         
+         
+    }
+
+ 
+    
+    
+}
---
+++ web/assets/js/Sign-Up.js
@@ -0,0 +1,29 @@
+async function SignUp() {
+
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+
+    const user = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password
+    };
+
+    const userJSON = JSON.stringify(user);
+
+    const response = await fetch(
+            "SignUp",
+            {
+                method: "POST",
+                body: userJSON,
+                header: {
+                    "Content-Type": "application/json"
+                }
+            }
+    );
+
+}
